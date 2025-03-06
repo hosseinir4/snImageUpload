b21311fb843da8a88cf43fe64306694df8ca11b8
unknown <hosseinir405@gmail.com>
2025-03-06 12:08:01 +0330

    fix problem on image url

--- src/SummerNoteIU.php
+++ src/SummerNoteIU.php
@@ -4,7 +4,7 @@ namespace SummerNoteIU;
 
 class SummerNoteIU
 {
-    public static function imageUpload($image,$dir)
+    public static function imageUpload($image,$dir,$url)
     {
         $dom = new \DomDocument();
         @$dom->loadHtml('<meta http-equiv="Content-Type" content="text/html; charset=utf-8">' .$image);
@@ -16,11 +16,13 @@ class SummerNoteIU
                 list($type, $data) = explode(';', $data);
                 list(, $data) = explode(',', $data);
                 $imgeData = base64_decode($data);
-                $image_url = $dir. DIRECTORY_SEPARATOR . time() . $item . '.png';
+                $image_name = time() . $item . '.png';
+                $image_dir = $dir. DIRECTORY_SEPARATOR . $image_name;
+                $image_url = $url.'/'.$image_name;
                 if(!is_dir($dir)){
                     mkdir($dir);
                 }
-                file_put_contents($image_url, $imgeData);
+                file_put_contents($image_dir, $imgeData);
                 $image->removeAttribute('src');
                 $image->setAttribute('src', $image_url);
             }
